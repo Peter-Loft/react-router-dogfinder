d67ed578649cd8beacb74cc6d5b8ff1f71abaf9a
Peter Loft <peterloft3@gmail.com>
2022-03-07 16:47:36 -0600

    Working on AJAX request, functioning, but with unending requests

--- src/DogList.js
+++ src/DogList.js
@@ -1,9 +1,35 @@
 import axios from "axios";
+import { useState } from "react";
 
 
-async function DogList(){
-    const dogList = await axios.get("localhost:5000/dogs")
-    console.log(dogList)
+/**
+ * 
+ */
+function DogList() {
+
+  const [dogList, setDogList] = useState([]);
+
+  async function listLoader() {
+      const response = await axios.get("http://localhost:5001/dogs");
+
+      setDogList([...response.data]);
+  }
+
+  listLoader();
+
+  console.log("dogList: ", dogList);
+
+
+  return (
+    <div className="DogList">
+      {dogList.map(dog => 
+        <div>
+          {dog.name}, {dog.facts[0]}
+        </div>
+        )}
+
+    </div>
+  );
 }
 
 export default DogList;
